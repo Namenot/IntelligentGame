ca225bbaf0efba115465365547621c6fc8c33593
Namenot <aaron.sastry2@gmail.com>
2018-02-03 16:13:14 +0100

    - added a header to operate the learning rules - added the blackbox

--- IntelligentGame/netdefaults.h
+++ IntelligentGame/netdefaults.h
@@ -1,5 +1,3 @@
-#pragma once
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -9,13 +7,21 @@
 #include "netcore.h"
 
 
-void blackbox(FILE *net1, FILE *net2, FILE *weightsbetween)
+void blackbox(double *layer1, double *layer2, FILE *weights)
 {
+
+  double *weight; // set size
+
+  getNet(weights, weight);
+
+  execNet(layer1, weight);
+  execNet(layer2, weight);
+
+  hebb(layer1, layer2, weights);
 
   /*
   - get the weights of the connections between the outputlayers (rdm generated)
   - use hebb'sch learning method to train em
-  - let them run both
   */
 
 }
---
+++ IntelligentGame/rules.h
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <CL/cl.h>
+
+#include "openfiles.h"
+#include "matrices.h"
+
+
+void hebb()
+{}
+
+void delta()
+{}
+
+void comp()
+{}
+
+void bprop()
+{}
