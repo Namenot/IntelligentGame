b1aefb0e7d2a3ca21fe18ef8c84614ec7d54c969
Namenot <aaron.sastry2@gmail.com>
2018-02-03 16:03:24 +0100

    some cleanup

---
+++ IntelligentGame/netdefaults.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <CL/cl.h>
+
+#include "netcore.h"
+
+
+void blackbox(FILE *net1, FILE *net2, FILE *weightsbetween)
+{
+
+  /*
+  - get the weights of the connections between the outputlayers (rdm generated)
+  - use hebb'sch learning method to train em
+  - let them run both
+  */
+
+}
+
+void loosenet()
+{}
+
+void defaultnet()
+{}
